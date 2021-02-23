41db03a2d31bdefd91e9b933eaeedb1a5c7cfd24
Kyle Davis <kyledavis957@gmail.com>
2021-02-23 17:53:36 -0500

    Fix pypi readme

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version="0.0.1",
+    version="0.0.1.2",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         "netmiko"
     ]
